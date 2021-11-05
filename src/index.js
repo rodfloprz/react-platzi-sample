520f08db0f3e77ce7d9b4091c3c0d9bfa3230c0d
Rodrigo Flores <rodrigoraulflores@gmail.com>
2021-11-04 22:53:26 -0600

    Fisrt commit from react project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+/* const e = React.createElement;
+
+function App(){
+  <h1 id="title">Hola React</h1>
+  return(
+    React.createElement('h1',{id: 'title'},'Oli React')
+    );
+}
+ReactDOM.render(e());
+*/
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  //<App/>
+  <App>
+    <h1>Buenaaaaaas!!</h1>
+  </App>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
